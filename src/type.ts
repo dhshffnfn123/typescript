f9bef1a9ea213f22cb6ceb1e82544647aedcce82
B H An <anbyunghoon0107@gamil.com>
2022-03-14 22:31:27 +0900

    타입의 종류와 특성

--- src/type.ts
+++ src/type.ts
@@ -40,3 +40,36 @@ console.log(myOrder["ord"]); // 123
 function getName(): string | null {
   return "good";
 }
+
+// never 타입은 절대 반환을 하지 않는 함수에 사용한다.
+// 절대로 실행이 종료되지 않는 함수나 오류를 발생시키기 위해서만 존재하는 함수를 예로 든다.
+
+// logger의 타입은 'never'이다.
+const logger = () => {
+  while (true) {
+    console.log("서버가 실행 중 입니다.");
+  }
+};
+
+// void 타입은 변수 선언이 아니라, 값을 반환하지 않는 함수를 선언하는데 사용된다.
+function logError(errorMessage: string): void {
+  console.error(errorMessage);
+}
+// never와 다르게 void는 실행을 완료하지만 값을 반환하지는 않는다.
+
+// 타입스크립트 내 타입 표기는 선택 사항이다.
+// 타입 표기가 없다면, 타입스크립트의 타입 검사기는 해당 타입을 유추한다.
+let name1 = "John Smith";
+let name2: string = "John Smith";
+// 두번째 행은 문법에 문제는 없지만 변수의 값이 문자열이므로 타입스크립트가 이미 유추했기 때문에 타입을 지정할 필요가 없다.
+// 타입스크립트 컴파일러가 유추 가능한 곳에 명시적으로 타입을 추가하는 것을 피해야 한다.
+
+const age1 = 25;
+function getTax(income: number): number {
+  return income * 0.15;
+}
+let yourTax = getTax(50000);
+
+// 타입스크립트는 문자열 리터럴을 타입으로 사용할 수 있다.
+let name3: "John Smith"; // 이 변수는 John Smith란 값을 가지며 값이 변경되면 오류가 발생한다.
+// name3 = "Mary Lau"; // error
